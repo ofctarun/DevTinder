281f1b6a895f045c2d572a2dfcc008d864bff94f
ofctarun <narayanashettitarun01@gmail.com>
2025-12-24 14:35:07 +0530

    Playing with the routes

--- src/app.js
+++ src/app.js
@@ -1,32 +1,36 @@
 const express = require("express");
-const { MongoClient } = require('mongodb');
-const uri = 'mongodb+srv://ofc_tarun:12345@namastenode.7fvpwio.mongodb.net/';
-const client = new MongoClient(uri);
+// const { MongoClient } = require('mongodb');
+// const uri = 'mongodb+srv://ofc_tarun:12345@namastenode.7fvpwio.mongodb.net/';
+// const client = new MongoClient(uri);
          
 const app = express();
 
-const getData = async () => {
-    await client.connect();
-    const database = client.db('HelloWorld');
-    const collection = database.collection("User");
+// const getData = async () => {
+//     await client.connect();
+//     const database = client.db('HelloWorld');
+//     const collection = database.collection("User");
 
-    const data = await collection.find({}).toArray();
-    return data;
-}
+//     const data = await collection.find({}).toArray();
+//     return data;
+// }
 
-app.get("/user",async (req ,res) => {
-    const arr = await getData();
-    res.json(arr);
-});
+// app.get("/user",async (req ,res) => {
+//     const arr = await getData();
+//     res.json(arr);
+// });
 
-
-app.post("/user",(req, res) => {
-    res.send("Data send to DB.");
+app.get("/abc/:userID",(req, res) => {
+    console.log(req.params);
+    res.send("DATA SEND");
 })
 
-app.use("/test" , (req ,res) => {
-    res.send("HELLO WORLD!!");
-})
+// app.post("/user",(req, res) => {
+//     res.send("Data send to DB.");
+// })
+
+// app.use("/test" , (req ,res) => {
+//     res.send("HELLO WORLD!!");
+// })
 
 app.listen(1818, () => {
     console.log("Running on Port 1818.");
