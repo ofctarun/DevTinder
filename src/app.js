d469772edbf21c1f35207a40630447a83dfdb956
ofctarun <narayanashettitarun01@gmail.com>
2025-12-24 14:00:11 +0530

    exploring get,post,delete

--- src/app.js
+++ src/app.js
@@ -1,8 +1,30 @@
 const express = require("express");
-
+const { MongoClient } = require('mongodb');
+const uri = 'mongodb+srv://ofc_tarun:12345@namastenode.7fvpwio.mongodb.net/';
+const client = new MongoClient(uri);
+         
 const app = express();
 
-app.use("/" , (req ,res) => {
+const getData = async () => {
+    await client.connect();
+    const database = client.db('HelloWorld');
+    const collection = database.collection("User");
+
+    const data = await collection.find({}).toArray();
+    return data;
+}
+
+app.get("/user",async (req ,res) => {
+    const arr = await getData();
+    res.json(arr);
+});
+
+
+app.post("/user",(req, res) => {
+    res.send("Data send to DB.");
+})
+
+app.use("/test" , (req ,res) => {
     res.send("HELLO WORLD!!");
 })
 
