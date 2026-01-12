4d995ca78952f1d3192f64beb38c49f8ed9071d6
ofctarun <narayanashettitarun01@gmail.com>
2026-01-12 12:06:03 +0530

    implemented Tailwind + Daisy UI + Navbar

--- FRONTEND/README.md
+++ FRONTEND/README.md
@@ -1,1 +1,8 @@
-# DEVTINDER
+# 
+
+- Create a Vite + React Application
+- Remove un-necessary code and create a Hello World app
+- Install Tailwind CSS
+- Install Daisy UI
+- Add NavBar component to App.jsx
+- 
--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -10,9 +10,9 @@ function App() {
         </div>
         <div className="flex gap-2">
           <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
-          <div className="dropdown dropdown-end">
+          <div className="dropdown dropdown-end mx-5">
             <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-              <div className="w-10 rounded-full">
+              <div className="w-10 rounded-full ">
                 <img
                   alt="Tailwind CSS Navbar component"
                   src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
