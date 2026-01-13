3da5d36f414627eefc02aa85306c3e5bf18bd12a
ofctarun <narayanashettitarun01@gmail.com>
2026-01-13 19:59:24 +0530

    vercel-with-local-backend

--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -9,7 +9,7 @@ import cors from "cors";
 
 const app = express();
 app.use(cors({
-    origin : "http://localhost:5173",
+    origin : ["http://localhost:5173","https://dev-tinder-pro.vercel.app/"],
     credentials : true,
 }));
 app.use(express.json());
