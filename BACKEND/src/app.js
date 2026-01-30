1c117085b3497184247cded58c8a199acceb8cd5
ofctarun <narayanashettitarun01@gmail.com>
2026-01-31 01:27:26 +0530

    try-2

--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -15,7 +15,7 @@ dotenv.config()
 
 const app = express();
 app.use(cors({
-    origin: ["http://localhost:5173", "https://dev-tinder-pro.vercel.app/"],
+    origin: ["http://localhost:5173", "https://dev-tinder-pro.vercel.app"],
     credentials: true,
 }));
 app.use(express.json());
